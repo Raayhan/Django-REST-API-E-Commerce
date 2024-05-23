421085a0402d10c7a29f0b8a17c7932e7ddc2fe3
Raayhan <rayhan.rakib@northsouth.edu>
2024-05-23 17:19:48 +0600

    Create Views for Product Detail

--- djackets_django/product/views.py
+++ djackets_django/product/views.py
@@ -1,8 +1,6 @@
-from django.shortcuts import render
-
+from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
 from .models import Product
 from .serializers import ProductSerializer
 
@@ -13,4 +11,16 @@ class LatestProductsList(APIView):
         serializer = ProductSerializer(products, many=True)
         return Response(serializer.data)
     
+
+class ProductDetail(APIView):
     
+    def get_object(self,category_slug,product_slug):
+        try:
+            return Product.objects.filter(category__slug=category_slug).get(slug=product_slug)
+        except Product.DoesNotExist:
+            raise Http404
+    
+    def get(self,request,category_slug,product_slug,format=None):
+        product = self.get.object(product_slug,category_slug)
+        serializer = ProductSerializer(products)
+        return Response(serializer.data)
