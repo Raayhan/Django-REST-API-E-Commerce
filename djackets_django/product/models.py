ebafe2805e20289554a0c309a53d4c9c1015adbb
Raayhan <rayhan.rakib@northsouth.edu>
2024-05-23 16:46:00 +0600

    Display Latest Products on Home Page

--- djackets_django/product/models.py
+++ djackets_django/product/models.py
@@ -63,13 +63,13 @@ class Product(models.Model):
                 return ''
     
     
-    def make_thumbnail(self,image, size=(300,200)):
+    def make_thumbnail(self,image, size=(600,400)):
         img = Image.open(image)
         img.convert('RGB')
         img.thumbnail(size)
 
         thumb_io = BytesIO()
-        img.save(thumb_io,'JPEG', quality=85)
+        img.save(thumb_io,'JPEG')
 
         thumbnail = File(thumb_io, name=image.name)
 
--- djackets_vue/src/views/HomeView.vue
+++ djackets_vue/src/views/HomeView.vue
@@ -1,15 +1,74 @@
 <template>
   <div class="home">
-   Home
+    <section class="hero is-small is-dark mb-6">
+      <div class="hero-body has-text-centered">
+        <p class="title mb-6">
+          Welcome to Djackets Online
+        </p>
+        <p class="subtitle">
+          The best jacket store online
+        </p>
+      </div>
+    </section>
+    <div class="columns is-multiline">
+      <div class="column is-12">
+        <h2 class="is-size-2 has-text-centered">
+          Latest Products
+        </h2>
+      </div>
+      <div class="column is-3" v-for="product in latestProducts" v-bind:key="product.id">
+
+        <div class="box">
+          <figure class="image mb-4 p-6">
+            <img :src="product.get_thumbnail" alt="Product Thumbnail">
+          </figure>
+          <h3 class="is-size-4">{{ product.name }}</h3>
+          <p class="is-size-6 has-text-gray">${{ product.price }}</p>
+          View Details
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 
+import axios from 'axios'
 export default {
   name: 'HomeView',
+  data() {
+    return {
+      latestProducts: []
+    }
+  },
   components: {
     
+  },
+  mounted() {
+    this.getLatestProducts()
+  },
+  methods: {
+    getLatestProducts() {
+      axios
+        .get('api/v1/latest-products')
+        .then(response => { this.latestProducts = response.data })
+        .catch(error => {
+          console.log(error)
+        })
+    }
   }
 }
 </script>
+
+<style scoped>
+  .image{
+    margin-top: -1.25rem;
+    margin-left: -1.25rem;
+    margin-right: -1.25rem;
+  }
+  .box{
+    height: 100%;
+  }
+  .image{
+    height: 75%;  }
+</style>
