39be80bab8528dd4003defb32f134eb4ea265228
Raayhan <rayhan.rakib@northsouth.edu>
2024-05-24 17:35:28 +0600

    Add to Cart show total length

--- djackets_vue/src/views/Product.vue
+++ djackets_vue/src/views/Product.vue
@@ -16,7 +16,7 @@
                         <input class="input" type="number" min="1" v-model="quantity">
                     </div>
                     <div class="control">
-                        <a class="button is-dark">Add to cart</a>
+                        <a class="button is-dark" @click="addToCart()">Add to cart</a>
                     </div>
                 </div>
             </div>
@@ -54,6 +54,16 @@ export default {
                 .catch(error => {
                     console.log(error)
                 })
+        },
+        addToCart() {
+            if (isNaN(this.quantity) || this.quantity < 1) {
+                this.quantity = 1
+            }
+            const item = {
+                product: this.product,
+                quantity: this.quantity
+            }
+            this.$store.commit('addToCart', item)
         }
     }
 
