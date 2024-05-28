b978d66a0d5e02a93d2bce7ceb7350736329a5fa
Raayhan <rayhan.rakib@northsouth.edu>
2024-05-28 11:51:41 +0600

    Inc Dec item and remove item from cart

--- djackets_vue/src/views/Cart.vue
+++ djackets_vue/src/views/Cart.vue
@@ -18,7 +18,7 @@
                         </tr>
                     </thead>
                     <tbody>
-                        <CartItem v-for="item in cart.items" v-bind:key="item.product.id" v-bind:initialItem="item" />
+                        <CartItem v-for="item in cart.items" v-bind:key="item.product.id" v-bind:initialItem="item" v-on:removeFromCart="removeFromCart" />
 
                     </tbody>
 
@@ -58,6 +58,13 @@ export default {
     },
     mounted() {
         this.cart = this.$store.state.cart
+    },
+    methods: {
+
+        removeFromCart(item) {
+            this.cart.items = this.cart.items.filter(i=>i.product.id !== item.product.id)
+            
+        }
     },
     computed: {
         cartTotalLength() {
